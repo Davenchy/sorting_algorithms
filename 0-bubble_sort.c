0ef49fcbe2dda596554eda2108fbf2d408912085
Fadi Asaad <firon1222@gmail.com>
2023-06-25 06:18:33 +0300

    add task 0: bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sort @array using the bubble sort algorithm
+ * @array: the array to sort
+ * @size: the number of elements in the array
+ * Note: Will print the array on each swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	char swapped = 0;
+
+	for (i = 0; i < size - 1; i++, swapped = 0)
+	{
+		for (j = 0; j < size - (1 + i); j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+		if (!swapped)
+			break;
+	}
+}
